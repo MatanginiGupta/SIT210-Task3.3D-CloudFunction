84d0cb749dc20353b431ecc405f3bafd1788d612
MatanginiGupta <133053094+MatanginiGupta@users.noreply.github.com>
2023-09-12 01:32:24 +0530

    Add files via upload

---
+++ Receivercode_3.3d.ino
@@ -0,0 +1,120 @@
+#include <ArduinoMqttClient.h>
+
+// Check the board type and include the appropriate WiFi library
+#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
+#include <WiFiNINA.h>
+#elif defined(ARDUINO_SAMD_MKR1000)
+#include <WiFi101.h>
+#elif defined(ARDUINO_ARCH_ESP8266)
+#include <ESP8266WiFi.h>
+#elif defined(ARDUINO_ARCH_ESP32)
+#include <WiFi.h>
+#endif
+
+// Define the network credentials
+char ssid[] ="matangini gupta's iphone";
+char pass[] = Matangini@1811;
+
+int light = 2; // Define the pin for controlling an LED
+// To connect with SSL/TLS:
+// 1) Change WiFiClient to WiFiSSLClient.
+// 2) Change port value from 1883 to 8883.
+// 3) Change broker value to a server with a known SSL/TLS root certificate
+//    flashed in the WiFi module.
+
+WiFiClient wifiClient; // Create a WiFi client object
+MqttClient mqttClient(wifiClient); // Create an MQTT client object
+
+const char broker[] = "mqtt-dashboard.com"; // Define the MQTT broker address
+int port = 1883; // Define the MQTT broker port
+const char topic[] = "Matangini/wave"; // Define the MQTT topic to subscribe to
+
+void setup() {
+  // Initialize serial communication and wait for the port to open:
+  Serial.begin(9600);
+  pinMode(light, OUTPUT); // Set the LED pin as an output
+
+  while (!Serial) {
+    ; // Wait for the serial port to connect. Needed for the native USB port only
+  }
+
+  // Attempt to connect to the WiFi network:
+  Serial.print("Attempting to connect to WPA SSID: ");
+  Serial.println(ssid);
+
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+    // Connection failed, retry
+    Serial.print(".");
+    delay(5000);
+  }
+
+  Serial.println("I'm connected to the network");
+  Serial.println();
+
+  // You can provide a unique client ID; if not set, the library uses Arduino-millis()
+  // Each client must have a unique client ID
+  // mqttClient.setId("clientId");
+
+  // You can provide a username and password for authentication
+  // mqttClient.setUsernamePassword("username", "password");
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("I'm connected to the MQTT broker!");
+  Serial.println();
+
+  Serial.print("Subscribing to topic: ");
+  Serial.println(topic);
+  Serial.println();
+
+  // Subscribe to a topic
+  mqttClient.subscribe(topic);
+  // Topics can be unsubscribed using:
+  // mqttClient.unsubscribe(topic);
+
+  Serial.print("Waiting for messages on topic: ");
+  Serial.println(topic);
+  Serial.println();
+}
+
+void loop() {
+  int messageSize = mqttClient.parseMessage();
+  if (messageSize) {
+    // We received a message, print out the topic and contents
+    Serial.print("Received a message with topic '' ");
+    Serial.print(mqttClient.messageTopic());
+    Serial.print("', length ");
+    Serial.print(messageSize);
+    Serial.println(" bytes:");
+
+    // Use the Stream interface to print the message contents
+    while (mqttClient.available()) {
+      Serial.print((char)mqttClient.read());
+    }
+    Serial.println();
+
+    // Blink the LED
+    digitalWrite(light, HIGH);   // Turn the LED on (HIGH is the voltage level)
+    delay(200);                       // Wait for a second
+    digitalWrite(light, LOW);    // Turn the LED off by making the voltage LOW
+    delay(200);
+    digitalWrite(light, HIGH);   // Turn the LED on (HIGH is the voltage level)
+    delay(200);                       // Wait for a second
+    digitalWrite(light, LOW);    // Turn the LED off by making the voltage LOW
+    delay(200);
+    digitalWrite(light, HIGH);   // Turn the LED on (HIGH is the voltage level)
+    delay(200);                       // Wait for a second
+    digitalWrite(light, LOW);    // Turn the LED off by making the voltage LOW
+    delay(200);
+
+    Serial.println();
+  }
+}
---
+++ sendercode_3.3D.ino
@@ -0,0 +1,85 @@
+/*
+  SIT210 Embedded System Development
+  Task 3.3D
+  I am Matangini Gupta, Student ID 2210994810.
+*/
+
+// Import the ArduinoMqttClient library.
+#include <ArduinoMqttClient.h>
+
+// If using a board with WiFiNINA, I include the following library.
+#if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
+#include <WiFiNINA.h>
+#elif defined(ARDUINO_SAMD_MKR1000)
+#include <WiFi101.h>
+#elif defined(ARDUINO_ARCH_ESP8266)
+#include <ESP8266WiFi.h>
+#elif defined(ARDUINO_ARCH_ESP32)
+#include <WiFi.h>
+#endif
+
+// I declared the SSID and password of my WiFi network.
+char ssid[] ="matangini gupta's iphone";
+char pass[] = Matangini@1811;
+
+// I declared the name of the MQTT broker.
+const char broker[] = "broker.mqttdashboard.com";
+
+// I declared the port of the MQTT broker.
+int port = 1883;
+
+// I declared the topic to publish the sensor readings to.
+const char topic[]  = "Matangini/wave";
+
+// I declared the interval (in milliseconds) between sensor readings.
+const long interval = 1000;
+
+// I declare the previous time a sensor reading was published.
+unsigned long previousMillis = 0;
+
+// I declared the number of sensor readings that have been published.
+int count = 0;
+
+void setup() {
+  // I initialized the serial port.
+  Serial.begin(9600);
+
+  // I connected to the WiFi network.
+  Serial.print("Attempting to connect to WPA SSID");
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("You're connected to the network");
+
+  // I connect to the MQTT broker.
+  Serial.print("Attempting to connect to the MQTTT broker: broker");
+  if (!mqttClient.connect(broker, port)) {
+    Serial.println("MQTT connection failed");
+    while (1);
+  }
+  Serial.println("You're connected to the MQTT broker!");
+}
+
+void loop() {
+  // I poll the MQTT client for incoming messages.
+  mqttClient.poll();
+
+  // I check if it's time to publish a sensor reading.
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // I publish the sensor reading.
+    mqttClient.beginMessage(topic);
+    mqttClient.print("Distance: ");
+    mqttClient.print(distance);
+    mqttClient.endMessage();
+
+    // I update the previous time.
+    previousMillis = currentMillis;
+
+    // I increment the number of sensor readings.
+    count++;
+  }
+}
