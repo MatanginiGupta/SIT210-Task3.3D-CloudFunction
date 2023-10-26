d18471d945747db3a41836f3491e38ce7359467f
MatanginiGupta <133053094+MatanginiGupta@users.noreply.github.com>
2023-10-26 10:32:28 +0530

    sendercode_3.3D.ino

--- sendercode_3.3D.ino
+++ sendercode_3.3D.ino
@@ -11,8 +11,8 @@
 #if defined(ARDUINO_SAMD_MKRWIFI1010) || defined(ARDUINO_SAMD_NANO_33_IOT) || defined(ARDUINO_AVR_UNO_WIFI_REV2)
 #include <WiFiNINA.h>
 
-const int trigger = 18                   # GPIO pin for ultrasonic sensor trigger
-const int echo = 24                      # GPIO pin for ultrasonic sensor echo
+const int trigger = 18                   // GPIO pin for ultrasonic sensor trigger
+const int echo = 24                      //GPIO pin for ultrasonic sensor echo
 
 // I declared the SSID and password of my WiFi network.
 char ssid[] ="matangini gupta's iphone";
